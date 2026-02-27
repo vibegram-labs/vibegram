960e9653b36f1940cbc2222801ad4a9d2c53d23d
Vibe Team <noreply@vibe-source.app>
2026-02-27 17:06:40 +0400

    fix(server): add :inets to extra_applications to fix :httpc not available error in production

--- server/mix.exs
+++ server/mix.exs
@@ -16,7 +16,7 @@ defmodule Vibe.MixProject do
   def application do
     [
       mod: {Vibe.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
